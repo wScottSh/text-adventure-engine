33c4a297a0903a31fc252a4035f45804710c170a
wScottSh <william.scott.sheppard@gmail.com>
2017-12-28 06:41:28 -0600

    abstracts form input to accept whatever is in the user form rather than a hard coded string

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ $(document).ready(function () {
   // console.log('jQuery ready');
 
   function Game () {
-    function Parser (userString) {
+    function Parser () {
       // console.log('parser sanity check');
 
-      this.userString = userString
+      // this.userString = userString
       this.approvedVerbs = ['go', 'get', 'use', 'look']
       this.approvedNouns = ['north', 'east', 'south', 'west']
 
       this.stringToArray = () => {
+        this.userString = $('#user-form').val()
         // this breaks the string into words for parsing
-        let lowerCaseString = this.userString.toLowerCase().replace(/[^\w\s]/g, '').replace(/\s+/g, ' ')
-        let workableArray = lowerCaseString.split(' ')
+        let workableArray = this.userString.toLowerCase().replace(/[^\w\s]/g, '').replace(/\s+/g, ' ').split(' ')
         return workableArray
       }
 
@@ -52,7 +52,7 @@ $(document).ready(function () {
         this.y = config.y
         this.z = config.z
         this.description = config.description
-        this.exits = {
+        this.exits = { // whay does this notation work in combination with the this.var notation above?
           north: config.north,
           northEast: config.northEast,
           east: config.east,
@@ -67,19 +67,19 @@ $(document).ready(function () {
         this.items = {}
       }
 
+      // The rooms
       this.room1 = new RoomMaker({
         x: 0,
         y: 0,
         z: 0,
-        north: [0, 1, 0],
+        north: [0, 1, 0], // when going north, move player to room with [x, y, z] coordinates
         east: [1, 0, 0],
         description: "This is the starting room. It's empty."
       })
     }
 
-    let fromForm
     this.myRooms = new Rooms()
-    this.myParser = new Parser(fromForm)
+    this.myParser = new Parser()
   }
 
   gameLogic = new Game()
