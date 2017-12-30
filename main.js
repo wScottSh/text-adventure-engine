adc4a05a17b8f86a6861b8c776232877d44d3e5f
wScottSh <william.scott.sheppard@gmail.com>
2017-12-30 16:28:11 -0600

    adds method findMyRoom() that looks compares player coordinates with room coordinates and returns the room name

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
   // console.log('jQuery ready');
   // debugger
+  const $compySays = (say) => {
+    $('#chat-log').append('<li class=\'compy-says\'>' + say + '</li>')
+  }
 
   function Parser () {
-    // console.log('parser sanity check');
-
     // this.userString = userString
     this.verbSynonyms = ['go', 'get', 'use', 'look']
     this.nounSynonyms = ['north', 'east', 'south', 'west']
@@ -12,7 +13,6 @@ $(document).ready(function () {
 
     this.stringToArray = () => {
       this.$userString = $('#user-input').val()
-      // this breaks the string into words for parsing
       let workableArray = this.$userString.toLowerCase().replace(/[^\w\s]/g, '').replace(/\s+/g, ' ').split(' ')
       return workableArray
     }
@@ -25,38 +25,27 @@ $(document).ready(function () {
       for (let i = 0; i < workableArray.length; i++) {
         for (let it = 0; it < words.length; it++) {
           if (workableArray[i] === words[it]) {
-            // console.log('Yay!!!! ' + workableArray[i] + ' DOES equal ' + words[it])
             return words[it]
-          } else {
-            // console.log(workableArray[i] + ' does not equal ' + words[it]);
           }
         }
       }
     }
 
     this.processInput = () => {
-      // this takes the above couple functions and actually parses the string, returning the thing that the computer needs to do
       let theVerb = this.findKeyword(this.verbSynonyms)
       let theNoun = this.findKeyword(this.nounSynonyms)
       let output = {verb: theVerb, noun: theNoun}
-      $('#chat-log').append('<li class="player-says">' + $('#user-input').val() + '</li>')
-      $('#user-input').val('')
-      console.log(output)
+      $('#chat-log').append('<li class=\'player-says\'>' + $('#user-input').val() + '</li>')
+      $('#user-input').val('') // clears the input field
       player.performAction(output)
-
-      // QUICK REFERENCE:
-      // gameLogic.myParser.processInput()[0] => processed verb
-      // gameLogic.myParser.processInput()[1] => processed noun
     }
   }
 
   function Rooms () {
-
     function RoomMaker (config) {
-      // this is the template of all the rooms stored in a decorator function
       this.coordinates = config.coordinates
       this.description = config.description
-      this.exits = { // why does this notation work in combination with the 'this.var' notation above?
+      this.exits = {
         north: config.north,
         northEast: config.northEast,
         east: config.east,
@@ -85,49 +74,75 @@ $(document).ready(function () {
       coordinates: [0, 0, 0],
       north: [0, 1, 0], // when going north, move player to room coords [x, y, z]
       east: [1, 0, 0],
-      description: "This is the starting room. It's empty."
+      description: 'This is the starting room. It\'s empty.'
     })
 
     this.room1 = new RoomMaker({
       coordinates: [0, 1, 0],
       south: [0, 0, 0],
-      description: "This is the northern room. It's empty."
+      description: 'This is the northern room. It\'s empty.'
     })
 
     this.room2 = new RoomMaker({
       coordinates: [1, 0, 0],
       west: [0, 0, 0],
-      description: "This is the easterly room. It's empty."
+      description: 'This is the easterly room. It\'s empty.'
     })
   }
 
-  function Player (location) {
-    this.location = location
+  function Player (coords) {
+    this.myCoords = coords
 
-    this.performAction = (words) => {
-      console.log('Performing input on the words ' + words.verb + ' and ' + words.noun)
-      if (words.verb === 'look') {
-        player.look()
+    this.findMyRoom = () => {
+      for (let i = 0; i < rooms.roomArray.length; i++) {
+        let roomName = 'room' + i
+        if (rooms[roomName]['coordinates'].join() === player.myCoords.join()) {
+          player.myRoom = rooms[roomName]
+          player.findExits(player.myRoom)
+        }
       }
     }
 
-    this.look = () => {
-      // this prints the room description and any exits
-      console.log('LOOK!')
-      $('#chat-log').append('<li class="compy-says">' + rooms.room0.description + '</li>')
+    this.findExits = (myRoom) => {
+      // use current room to determine which exits are available for me to move through
     }
 
-    this.move = () => {
+    this.performAction = (words) => {
+      console.log('Performing action on the words ' + words.verb + ' and ' + words.noun)
+      if (words.verb === 'look') {
+        player.look(words)
+      } else if (words.verb === 'go') {
+        player.move(words)
+      } else if (words.verb === 'get') {
+        player.get(words)
+      } else if (words.verb === 'use') {
+        player.use(words)
+      }
+    }
+
+    this.look = (words) => {
+      let room = rooms.room0 // replace this hard coded variable with player's current room
+      $compySays(room.description)
+      // and the exits are?
+    }
+
+    this.move = (words) => {
       // moves the player location to the coordinates listed in the direction typed
-      console.log('MOVE!')
+      if (words.noun !== undefined) {
+        $compySays('In the future, you\'ll actually move ' + words.noun)
+        // write the code to actually move in that direction :)
+        // also check if the direction to move is a valid direction to move
+      } else {
+        $compySays('Which direction do you want to go? Available directions are (blah blah blah)')
+      }
     }
 
-    this.use = () => {
-      // this is the interaction function. Either items in your inventory, or items in the world that are interactable but not pickupable.
+    this.use = (words) => {
+      // this is the interaction function. Either items in your inventory, or items in the world that are interactable but not pick-up-able.
       console.log('USE!')
     }
 
-    this.get = () => {
+    this.get = (words) => {
       // this is the function that adds items to your inventory
       console.log('GET!')
     }
@@ -137,6 +152,7 @@ $(document).ready(function () {
   parser = new Parser()
   player = new Player([0, 0, 0])
   rooms.roomLister(2)
+  player.findMyRoom()
 
   $('#user-form').submit(function (event) {
     parser.processInput()
